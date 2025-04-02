bef0ad70fb8e320f6b878af91db8909a8083df2e
TheMaZe123 <shitov.1497@bk.ru>
2025-04-02 16:20:37 +0300

    fix: rename methods into user_service

--- app/api/routers/user.py
+++ app/api/routers/user.py
@@ -7,14 +7,14 @@ router = APIRouter(prefix="/users", tags=["Users"])
 @router.post("/", response_model=UserResponse)
 def register_user(user_data: UserCreate = Depends, service: UserService = Depends(UserService)):
     try:
-        return service.register_user(user_data)
+        return service.add(user_data)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 @router.patch("/{user_id}", response_model=UserResponse)
 def update_user(user_id: int, user_data: UserUpdate, service: UserService = Depends(UserService)):
     try:
-        return service.update_user_info(user_id, user_data)
+        return service.update(user_id, user_data)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     
