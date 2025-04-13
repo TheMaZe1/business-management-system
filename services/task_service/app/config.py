37f3653c6f8c3616dd5960bebe4b456219c3d822
TheMaZe123 <shitov.1497@bk.ru>
2025-04-14 02:31:15 +0300

    add: initial commit for task service

---
+++ services/task_service/app/config.py
@@ -0,0 +1,16 @@
+import os
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"  # Алгоритм для JWT
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30  # Время жизни токена
+    
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
---
+++ services/task_service/app/database/db.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from app.config import settings  # предполагается, что настройки из .env
+
+DATABASE_URL = settings.DATABASE_URL  # например: "postgresql+asyncpg://user:password@localhost/dbname"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_db_session() -> AsyncSession: # type: ignore
+    async with async_session() as session:
+        yield session
